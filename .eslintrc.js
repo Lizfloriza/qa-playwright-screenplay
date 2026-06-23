0a80566d0e6942de22556c1769c0ba01e2f5ea74
Lizfloriza <58743862+Lizfloriza@users.noreply.github.com>
2026-06-22 22:49:44 -0500

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,17 @@
-import js from '@eslint/js';
-
-export default [
-  js.configs.recommended,
-  {
-    files: ['**/*.ts'],
-    rules: {
-      'no-unused-vars': 'warn',
-    },
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  env: {
+    node: true,
+    es2022: true,
   },
-];
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'no-undef': 'off',
+  },
+};
